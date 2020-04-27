dd2a4e5e69a3bac6e4ae8a14dac29aeb4ed14122
rashika0601 <61932570+rashika0601@users.noreply.github.com>
2020-04-27 14:51:15 +0530

    Using if-else condition

---
+++ if-else/leapyear.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+read -p "Enter the year:" year
+# Long code
+if [[ $(($year%4)) -eq 0 ]]
+then
+	echo "Year is leap year"
+elif [[ $(($year%100)) -eq 0 ]]
+then 
+	echo "Year is not leap year"
+elif [[ $(($year%400)) -eq 0 ]]
+then
+	echo "Year is leap year"
+else
+	echo "Year is not leap year"
+fi
+# short code
+if [[ $(($year%4)) -eq 0 || ($(($year%100)) -ne 0  && $(($year%400)) -eq 0) ]]
+then
+	echo "Leap"
+else
+	echo "Not"
+fi
